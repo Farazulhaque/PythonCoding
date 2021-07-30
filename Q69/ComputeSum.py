269047469ec48167be6ef9986f8abad7e933df0c
Faraz <farazattari786@gmail.com>
2021-07-30 07:49:50 +0530

    compute sum of odd numbers

---
+++ Q69/ComputeSum.py
@@ -0,0 +1,19 @@
+def compute_sum(filename):
+    try:
+        infile = open(filename, "r")
+        sums = 0
+        count = 0
+        for line in infile:
+            line = line.strip()
+            num = float(line)
+            if num % 2 != 0:
+                sums += num
+                count += 1
+
+        print("Sum of the odd numbers is: {:.2f}".format(sums))
+        print("Average is: {:.2f}".format(sums/count))
+    except:
+        print("ERROR: File can't open")
+
+
+compute_sum("data.txt")
