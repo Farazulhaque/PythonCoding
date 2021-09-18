c066a27d7e5d019c103c300ba51724d377e2d69a
Faraz <farazattari786@gmail.com>
2021-09-18 11:10:56 +0530

    square root of 1 to 10

---
+++ Q83/sqrt_loop.py
@@ -0,0 +1,8 @@
+# import math module to use sqrt function
+import math
+
+# loop start from 1 to 11 - 1
+for i in range(1, 11):
+    # print i and square root of i upto 2 decimal places
+    print(f"{i} : {(math.sqrt(i)):.2f}")
+
