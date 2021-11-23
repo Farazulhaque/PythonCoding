f5ed2c8efd6548667326512cb86ae6fe5b9200cf
Farazulhaque <farazattari786@gmail.com>
2021-11-23 08:01:16 +0530

    sales prediction

---
+++ Q128/salesPrediction.py
@@ -0,0 +1,8 @@
+print("*"*16)
+print("Sales Prediction")
+print("*"*16)
+
+total_sales = int(input("Enter the expected total sales: RM "))
+profit = total_sales * 0.35
+print(f"The expected profit ids RM {profit}")
+print(f"The expected profit ids RM {profit:.2f}")
