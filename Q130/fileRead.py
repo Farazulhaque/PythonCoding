b3c30d3042642d9ecaddc5045b55cabd06f111da
Farazulhaque <farazattari786@gmail.com>
2021-11-25 21:01:01 +0530

    read a file and write to a file

---
+++ Q130/fileRead.py
@@ -0,0 +1,17 @@
+def main():
+    print("Test.txt file before adding 'hello world' at the end.")
+    with open("test.txt", "r") as file:
+        for line in file:
+            print(line)
+
+    with open("test.txt", "a") as file:
+        file.write("hello world")
+F
+    print("\nTest.txt file after adding 'hello world' at the end.")
+    with open("test.txt", "r") as file:
+        for line in file:
+            print(line)
+
+
+main()
+
